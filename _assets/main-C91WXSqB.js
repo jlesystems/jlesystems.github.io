5cb325c050537173f2e37f9eed4d3dea57af6b1b
Ing. Julian Lamplmair - JLE Systems <julian.lamplmair@jle.systems>
2026-01-20 10:15:59 +0100

    Deploy 2026-01-20 10:15

---
+++ _assets/main-C91WXSqB.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index-DIdCSoZA.js";import{A as o}from"./article-list-YllbMGDX.js";import{u as s}from"./index-DSqa7jBv.js";import"./preload-BYIKhQBs.js";import"./smart-link-nit6UY_6.js";import"./button-5CML60KB.js";function r(n){const t={h1:"h1",hr:"hr",p:"p",strong:"strong",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Articles"}),`
+`,e.jsxs(t.p,{children:["[SUBTITLE] ",e.jsx(t.strong,{children:"Guides, architectures and tips"})]}),`
+`,e.jsx(t.p,{children:"Here you'll find in-depth analyses and practical examples of my work at JLE Systems. Whether it's smart home control, software architecture, or IoT solutions: I document what's technically feasible when you think like an engineer and build like a developer."}),`
+`,e.jsx(t.hr,{}),`
+`,e.jsx(o,{})]})}function d(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{d as default};
