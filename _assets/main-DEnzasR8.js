cb2c94e0c534eab37ea32510f65bc965dbda6551
Ing. Julian Lamplmair - JLE Systems <julian.lamplmair@jle.systems>
2026-01-20 17:45:48 +0100

    Deploy 2026-01-20 17:45

--- _assets/main-DEnzasR8.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index-DJE85yAI.js";import{A as s}from"./article-list-BcPm0bJS.js";import{u as i}from"./index-BQF6dOX3.js";import"./preload-CtHSH9i2.js";import"./smart-link-MPgwwfOR.js";import"./button-7z_dqc7n.js";function r(t){const n={h1:"h1",hr:"hr",p:"p",strong:"strong",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{children:"Artikel"}),`
-`,e.jsxs(n.p,{children:["[SUBTITLE] ",e.jsx(n.strong,{children:"Leitfäden, Architekturen und Tipps"})]}),`
-`,e.jsx(n.p,{children:"Hier finden Sie Deep-Dives und Praxisbeispiele meiner Arbeit bei JLE Systems. Ob Smart Home-Steuerung, Software-Architektur oder IoT-Lösungen: Ich dokumentiere, was technisch machbar ist, wenn man wie ein Ingenieur denkt und wie ein Entwickler baut."}),`
-`,e.jsx(n.hr,{}),`
-`,e.jsx(s,{})]})}function d(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r(t)}export{d as default};
---
+++ _assets/main-Dza3OHni.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index-DGJdGg6m.js";import{A as o}from"./article-list-CtEJCNS-.js";import{u as s}from"./index-CnwEelhj.js";import"./preload-CSbjO1tw.js";import"./smart-link-s0j_GtUz.js";import"./button-C5HXS48H.js";function r(n){const t={h1:"h1",hr:"hr",p:"p",strong:"strong",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Articles"}),`
+`,e.jsxs(t.p,{children:["[SUBTITLE] ",e.jsx(t.strong,{children:"Guides, architectures and tips"})]}),`
+`,e.jsx(t.p,{children:"Here you'll find in-depth analyses and practical examples of my work at JLE Systems. Whether it's smart home control, software architecture, or IoT solutions: I document what's technically feasible when you think like an engineer and build like a developer."}),`
+`,e.jsx(t.hr,{}),`
+`,e.jsx(o,{})]})}function d(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(r,{...n})}):r(n)}export{d as default};
